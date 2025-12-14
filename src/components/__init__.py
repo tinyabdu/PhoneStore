0cdcdf6f1621908187cc8bcdd3bdb90c9b5499be
abdullahiharun908@gmail.com <112319829+tinyabdu@users.noreply.github.com>
2025-12-14 02:05:03 +0100

    change

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -1,10 +1,6 @@
-from .modal import NotificationModal
 from .container import Box
-from .header import Header
 from .sidebar import Sidebar
 __all__ = [
-    "NotificationModal",
     "Box",
-    "Header",
     "Sidebar"
 ]
